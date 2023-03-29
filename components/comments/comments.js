bcee037c9759d3a52d432553c4a410f43cb5bc45
samaksh mittal <sam.mittal44@gmail.com>
2023-03-30 01:36:33 +0530

    chore: fetch all comments and filter rootComments

--- components/comments/comments.js
+++ components/comments/comments.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getComments as fetchComments } from "@/utils/localAPI";
 
-const Comments = () => {
-  return <div>Comments</div>;
+const getComments = () => {
+  const comments = fetchComments();
+  console.log(comments);
+  return comments;
+};
+
+const Comments = (props) => {
+  const { currentUserId = 1 } = props;
+  const [comments, setComments] = useState([]);
+  const rootComments = comments.filter(({ parentId }) => parentId === null);
+  console.log("s", rootComments);
+
+  useEffect(() => {
+    setComments(getComments());
+  }, []);
+
+  return (
+    <div className={"comments"}>
+      <h3 className={"comments-title"}>{"Comments"}</h3>
+      <div className="comments-container"></div>
+    </div>
+  );
 };
 
 export default Comments;
