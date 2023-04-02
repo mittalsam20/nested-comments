9a2fd4cd1fdd936b7c9a2ed489a366908e1493ee
samaksh mittal <sam.mittal44@gmail.com>
2023-04-03 02:47:21 +0530

    feat: succesfully migrated from next.js to react.js

--- nested_comments_react/src/App.js
+++ nested_comments_react/src/App.js
@@ -1,21 +1,8 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Comments from "./components/comments";
+import "./styles/global.css";
+
+const App = () => {
+  return <Comments />;
+};
 
 export default App;
